31f476ac7a8f1bdacbbff6409fea0e32c70afaab
Serhiy Oliyarnik <sergoliarnik@gmail.com>
2024-01-01 17:15:47 +0200

    Initial commit

---
+++ src/main/java/org/example/bookease/BookEaseApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookEaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bookease/BookEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
